4bccf9c0d37c29a516e53e04bb5fa735a779385b
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2025-01-29 14:47:23 +0530

    Add files via upload

---
+++ switchoperator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,num3,operator;
+    printf("\n,Enter the first number:");
+    scanf("%d",&num1);
+    printf("\n,Enter the second number:");
+    scanf("%d",&num2);
+    printf("\n,Enter the operator:");
+    operator=getchar();
+    switch(operator)
+    {
+    case'+':
+        printf("%d+%d=%d",num1,num2,num1+num2);
+        break;
+    case'-':
+        printf("%d-%d=%d",num1,num2,num1-num2);
+        break;
+    case'*':
+        printf("%d*%d=%d",num1,num2,num1*num2);
+        break;
+    case'/':
+        printf("%d/%d=%d",num1,num2,num1/num2);
+        break;
+    case'%':
+        printf("%d % %d=%d",num1,num2,num1%num2);
+        break;
+        default:
+        printf("Invalid operator");
+        break;
+        return 0;
+    }
+}
+
+
