9f09d9205c8a6990ae1fa523f337bc94d498ec28
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2025-01-29 15:59:09 +0530

    Update switchoperator.c

--- switchoperator.c
+++ switchoperator.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int num1,num2,num3,operator;
+    int num1,num2,num3;
+    char operator;
     printf("\n,Enter the first number:");
     scanf("%d",&num1);
     printf("\n,Enter the second number:");
     scanf("%d",&num2);
     printf("\n,Enter the operator:");
-    operator=getchar();
+    scanf("%c",%operator);
     switch(operator)
     {
     case'+':
@@ -23,7 +24,7 @@ int main()
         printf("%d/%d=%d",num1,num2,num1/num2);
         break;
     case'%':
-        printf("%d % %d=%d",num1,num2,num1%num2);
+        printf("%d %% %d=%d",num1,num2,num1%num2);
         break;
         default:
         printf("Invalid operator");
